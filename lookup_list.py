1399b61485caba46735a68960c5463cbea7965cd
samfriedmann <40502468+samfriedmann@users.noreply.github.com>
2020-10-04 01:46:30 -0400

    Update lookup_list.py
    Lots of imporvements, especially to error-handling and reporting data back to the user.

--- lookup_list.py
+++ lookup_list.py
@@ -1,8 +1,8 @@
 import requests
 import json
-from requests.exceptions import HTTPError
 import wikipedia
 import re
+import os
 
 def lookup(word, num_sentences):
     try: # try Google dictionarty first
@@ -14,37 +14,35 @@ def lookup(word, num_sentences):
         definition = definition[0].lower() + definition[1:]  # lowercase the first letter
         print("The definition of "+word+" is: "+definition)
 
-    except HTTPError: # if there's an error (can't find the word on Google Dictionary)
-        # print(f'   HTTP error occurred: {http_err}') # might need this for diagnosing HTTP errors
+    except Exception as err: # if there's an error (can't find the word on Google Dictionary)
+        # print(f'   HTTP error occurred: {http_err}') # might need this for diagnosing HTTP errors # (requires 'from requests.exceptions import HTTPError')
+        print(f'   An error occurred getting the definition from Google: {err}')
         print("   Definition not found on Google for '"+word+"'. Checking Wikipedia instead...")
         try:
             definition=wikipedia.summary(word, sentences=num_sentences, auto_suggest=True, redirect=True) # try to find the word on Wikipedia
             print("According to Wikipedia: "+definition)
         except Exception: # if a page isn't immedietly available (disambiguation, etc)
             try:
-                print("   Couldn't find a Wikipedia article for '"+word+"'. Searching Wikipedia for relevant articles...")
+                print("   Couldn't find a Wikipedia article called '"+word+"'. Searching Wikipedia for relevant articles...")
                 actual_article=wikipedia.search(word)[0] # suggest the result
-                print("   Wikipedia returned '"+actual_article+"' as the best article. Getting summary of "+actual_article+"...")
+                print("   Wikipedia returned '"+actual_article+"' as the best article. Getting summary of '"+actual_article+"'...")
                 definition=wikipedia.summary(actual_article, sentences = num_sentences, auto_suggest=False, redirect=True) # get a summary of that article
             except Exception:
                 definition="No definition found."
             print("According to Wikipedia: "+definition)
 
         if ' is ' in definition or ' was ' in definition or ' are ' in definition or ' were ' in definition: # if the Wikipedia definition contains one of these words
-            print("   Attempting to isolate the definition...")
-            definition = re.split(" is | was | are | were", definition, maxsplit=1)[1] # split the definition once and assign the second part to definition
-            print("   New Wikipedia summary: "+definition)
-
-    except Exception as err: # print extraneous errors
-        print(f'   Other error occurred: {err}')
+            print("   Isolating the definition...")
+            definition = re.split(" is | was | are | were", definition, maxsplit=1)[1] # split the definition once and assign the second part ([1]) to definition
 
     if '==' in definition: # if the definition contains dividers
         print("   Detected new heading divider. Isolating preceding text...")
         definition = definition.split('==')[0] # split the definition once and assign the first part to definition
         print("Extracted: "+definition)
 
-    print("   Cleaning up definition to remove excess characters and end periods...")
+    print("   Cleaning up definition to remove excess characters, end periods, and line breaks...")
     definition = definition.strip() # strip any unnecessary start or end spaces
+    definition = definition.replace('\n', ' ')
     definition = definition[:-1] # remove end period
     return definition # return the definition of the word
 
@@ -52,23 +50,25 @@ def main(num_sentences): # main method
     try: 
         print("Opening words file...")
         file=open('words.txt', encoding='utf8') # open the words file
-    except Exception:
+    except Exception as err:
+        print(err) # print errors
         print("File not found. Please ensure words.txt is in the root directory.")
+        print("Currently working in "+str(os.getcwd())) # print info about local directory and files to help diagnose missing words.txt
+        print("Only files in this directory are: "+str(os.listdir()))
         input("\nPress Enter to continue.")
 
-    # words = [] # declare an empty words list # might not need this idk yet
-
-    words=file.readlines() # append each line of text in the file to the words list
+    words = [line for line in file.readlines() if line.strip()] # append each non-empty line of text in the file to the words list
     file.close() # close the file
 
     words = [word.strip() for word in words] # strip each line to remove unessecary characters
 
-    print("There are the terms I read:") # print the words list
+    print("\nThere are the terms I read:") # print the words list
     print(words)
     if num_sentences!=1:
-        print("I will get definitions from Wikipedia with "+str(num_sentences)+" sentences.") # indicate the number of sentences getting from Wikipedia
+        print("\nI will get definitions from Wikipedia with "+str(num_sentences)+" sentences.\n") # indicate the number of sentences getting from Wikipedia
     else:
-        print("I will get definitions from Wikipedia with 1 sentence.") # indicate the number of sentences getting from Wikipedia
+        print("\nI will get definitions from Wikipedia with 1 sentence.\n") # indicate the number of sentences getting from Wikipedia
+    
     definitions=[] # declare an empty definitions list
 
     bad_chars = ['“', '”'] # characters to remove before looking up
@@ -82,20 +82,34 @@ def main(num_sentences): # main method
         print("Getting definition of '"+word+"' to add to dictionary...")
         definitions.append(lookup(word, num_sentences)) # look up the word (using num_sentences sentences) and append it to the dictionary list
 
-    definitions_file = open('wordsDefined.txt', 'w') # open the destination file for writing
+    errors = [] # declare an empty list of errors
+
+    for i in range(len(definitions)): # for each definition in the list
+        if definitions[i] == "No definition found": # find each term without a definition
+            errors.append(words[i]) # add each term without a definition to the list
+
+    successful_words = len(words)-len(errors) # calculate the number of successfully defined terms
+
+    definitions_file = open('wordsDefined.txt', 'w', encoding='utf8') # open the destination file for writing
     for i in range(len(words)): # for each element in the words list
         definitions_file.writelines(words[i]+" - "+definitions[i]+"\n") # write each 'word - definition' line
-    print("\nSuccessfully wrote "+str(len(words))+" terms and definitions to wordsDefined.txt.")
+    print("\nSuccessfully wrote "+str(successful_words)+" terms and definitions to wordsDefined.txt.")
+    if len(errors) == 1: # if there were errors, print them
+        print("There was 1 term I could not define: '"+str(errors[0])+"'")
+    elif len(errors) > 0:
+        print("There were "+str(len(errors))+" terms I could not define: "+str(errors))
+
     definitions_file.close() # close the file
+
 
 while True:
     try:
         num_sentences = int(input("Enter number of sentences to get from Wikipedia: ")) # get number of sentences from user
-    except Exception: # if it is not an integer, return to the start of the loop
-        print("Number must be an integer.")
-        continue
-    else: # if it is an integer, break the loop
-        break
+    except Exception: # if it is not an integer,
+        print("Number must be an integer.") # inform the user and..
+        continue # return to the start of the loop
+    else: # if it is an integer,
+        break # break the loop
 
 main(num_sentences) # call the main function (parameter is number of sentences to get from Wikipedia)
 
